60550c0c28789be56ebf8da0d90d23903e669a37
leoxk93 <leo_xk93@hotmail.com>
2022-04-12 20:02:48 -0300

    cambios

---
+++ numeros_pares.php
@@ -0,0 +1,30 @@
+<?php
+
+for($i = 1; $i <= 100; $i++){
+  $resultado = $i;
+  echo "<p>$resultado</p>";
+}
+
+?>
+
+
+<?php
+
+for($i = 2; $i <= 100; $i++){
+  $resultado = $i++;
+  echo "<p>$resultado</p>";
+}
+
+?>
+
+
+
+<?php
+ $resultado = 1;
+
+for($i = 1; $i <= 100; $i++){
+  $resultado = $i++;
+  echo "<p>$resultado</p>";
+}
+
+?>
