68f56fd30a387c379bf670754a0e7351f1bfc9e6
leoxk93 <leo_xk93@hotmail.com>
2022-04-19 20:58:56 -0300

    cambios

---
+++ empleados.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 py-5">
+                <h1>Lista de Empleados</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-hover border">
+                    <thead>
+                        <tr>
+                            <th>DNI</th>
+                            <th>Nombre</th>
+                            <th>Sueldo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>66655588</td>
+                            <td>DAVID MARTINEZ</td>
+                            <td>70.550,25</td>
+                        </tr>
+                        <tr>
+                            <td>33344422</td>
+                            <td>JULIAN ALVAREZ</td>
+                            <td>80.550,25</td>
+                        </tr>
+                        <tr>
+                            <td>55544422</td>
+                            <td>FRANCO ARMANI</td>
+                            <td>90.550,25</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
