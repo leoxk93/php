183f38bb6882983067f6ce490e0eec4318eb0e60
leoxk93 <leo_xk93@hotmail.com>
2022-04-28 20:09:00 -0300

    cambios

--- clinica.php
+++ clinica.php
@@ -9,20 +9,21 @@ $aPacientes[] = array(
     "dni" => "33.765.012",
     "nombre" => "Ana Acuña",
     "edad" => 45,
-    "peso" => 81.50,
+    "peso" => 81.50
 );
 $aPacientes[] = array(
     "dni" => "23.684.385",
     "nombre" => "Gonzalo Bustamante",
     "edad" => 66,
-    "peso" => 79,
+    "peso" => 79
 );
 $aPacientes[] = array(
     "dni" => "11.568.778",
     "nombre" => "Martín Perez",
     "edad" => 26,
-    "peso" => 77,
+    "peso" => 77
 );
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
